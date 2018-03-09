558e2de39017fcba4a99644fbb0acdddab0807fc
Lin Clark <lclark@mozilla.com>
2018-03-09 14:02:24 -0500

    Clear out old site

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  var body = $('body');
-  var viewportHeight = $(window).height();
-  if (viewportHeight > body.outerHeight()) {
-    var padding = body.outerHeight() - body.height();
-    body.height(viewportHeight - padding);
-  }
-})(jQuery);
